1fe64a09f2d4b4206f78806d5110e804bfdb98e1
Crislp12 <cristopher.picopinos@gmail.com>
2024-10-16 23:19:44 -0400

    feat : refactor filtering system

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,8 +1,8 @@
 async function fetchWorks() {
   try {
     const response = await fetch('http://localhost:5678/api/works');
-    const userData = await response.json();
-    return userData; // this returns the array of objects
+    const works = await response.json();
+    return works; // this returns the array of objects
   } catch (error) {
     console.log(error);
   }
@@ -20,88 +20,62 @@ async function fetchCategories() {
 
 // Functions to render images on home page
 
-function createImg(element, categories) {
-  const card = document.createElement('figure');
-
-  for (let i = 0; i < categories.length; i++) {
-    if (categories[i].id === element.categoryId) {
-      const name = categories[i].name.replace(/\s+/g, '-');
-      card.classList.add(`${element.userId}`, `${name}`);
-    }
-  }
-
-  const img = document.createElement('img');
-  img.src = element.imageUrl;
-  img.alt = element.title;
-
-  const workTitle = document.createElement('figcaption');
-  workTitle.textContent = element.title;
-
-  card.appendChild(img);
-  card.appendChild(workTitle);
-
-  return card;
-}
-
-async function renderImages(users) {
-  const data = await fetchWorks();
-  const categories = await fetchCategories();
-  const gallery = document.getElementsByClassName('gallery');
-
-  data.forEach((element) => {
-    const card = createImg(element, categories);
-    gallery[0].appendChild(card);
-  });
-}
-
 document.addEventListener('DOMContentLoaded', async () => {
-  const userEmails = await renderImages();
+  const awaitWorks = await fetchWorks();
+  const awaitCategories = await fetchCategories();
+  const section = document.getElementById('portfolio');
+  const gallery = document.getElementsByClassName('gallery');
+  const buttonsContainer = document.getElementsByClassName('filter-buttons-container');
+
+  // Add the HTML to the div
+
+  const buttonFilters = `
+          <button class="button-filter all">All</button>
+          <button class="button-filter objects">Objects</button>
+          <button class="button-filter apartments">Apartments</button>
+          <button class="button-filter hotels-&-restaurants">Hotels & Restaurant</button> `;
+
+  buttonsContainer[0].innerHTML = buttonFilters;
+
+  let works = '';
+
+  awaitWorks.forEach((work) => {
+    const category = awaitCategories.find((element) => element.id === work.category.id).name.replace(/\s+/g, '-');
+
+    works += `
+    <figure class="work-item ${category.toLowerCase()} ${work.id}">
+      <img src=${work.imageUrl} alt=${work.title} />
+      <figcaption>${work.title}</figcaption>
+    </figure> `;
+  });
+
+  gallery[0].innerHTML = works;
+
+  const buttons = document.querySelectorAll('.button-filter');
+  const renderWorks = document.querySelectorAll('.work-item');
+
+  // Loop through all buttons and add event listeners dynamically
+
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const category = button.className.split(' ')[1]; 
+      
+      if (category === 'all') {
+        gallery[0].innerHTML = works;
+      } else {
+        let filterWorks = ''; 
+        
+        renderWorks.forEach((renderWork) => {
+          // Check if the job category matches the button category
+          if (renderWork.className.split(' ')[1].toLowerCase() === category.toLowerCase()) {
+            filterWorks += renderWork.outerHTML; // Add matching job's HTML to filterWorks
+          }
+        });
+  
+        gallery[0].innerHTML = filterWorks; // Display filtered works
+      }
+    });
+  });
+  
 });
-
-// button filters
-
-const allBtn = document.querySelector('.all');
-const objectsBtn = document.querySelector('.objects');
-const aparmentsBtn = document.querySelector('.aparments');
-const hotelsAndRestaurantsBtn = document.querySelector('.hotels-and-restaurants');
-
-const objects = document.getElementsByClassName('Objects');
-const aparments = document.getElementsByClassName('Apartments');
-const hotels = document.getElementsByClassName('Hotels-&-restaurants');
-
-function addClassName(array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i].classList.add('display');
-  }
-}
-
-function removeClassName(array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i].classList.remove('display');
-  }
-}
-
-allBtn.addEventListener('click', ()=> {
-  removeClassName(objects);
-  removeClassName(aparments);
-  removeClassName(hotels);
-})
-
-objectsBtn.addEventListener('click', () => {
-  removeClassName(objects);
-  addClassName(aparments);
-  addClassName(hotels);
-});
-
-aparmentsBtn.addEventListener('click', () => {
-  removeClassName(aparments);
-  addClassName(hotels);
-  addClassName(objects);
-});
-
-hotelsAndRestaurantsBtn.addEventListener('click', ()=> {
-  removeClassName(hotels);
-  addClassName(aparments);
-  addClassName(objects)
-})
-
