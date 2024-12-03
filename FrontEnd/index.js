7631ba749b88c9e0ae289581c9244a916d1329a4
Crislp12 <cristopher.picopinos@gmail.com>
2024-12-03 13:42:40 -0500

    feat: added a modal form, where users can upload imgs

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       .name.replace(/\s+/g, '-')
       .toLowerCase();
     works += `
-    <figure class="work-item ${category} ${work.id}">
+    <figure data-custom="${work.id}" class="work-item ${category}">
       <img src=${work.imageUrl} alt=${work.title} />
       <figcaption>${work.title}</figcaption>
     </figure> `;
@@ -85,7 +85,10 @@ document.addEventListener('DOMContentLoaded', async () => {
   const openButton = document.querySelector('[data-open-modal]');
   const closeButton = document.querySelector('[data-close-modal]');
   const modal = document.querySelector('[data-modal]');
-  const editGallary = document.querySelector('.edit-gallary');
+  const editGallary = document.querySelector('.edit-gallery');
+  const addPhotoButton = document.getElementById('add-photo');
+  const fileInput = document.getElementById('input-file');
+  const arrow = document.querySelector('.arrow-edit');
 
   openButton.addEventListener('click', () => {
     modal.showModal();
@@ -95,7 +98,39 @@ document.addEventListener('DOMContentLoaded', async () => {
     modal.close();
   });
 
-  getEditImgs(worksData, editGallary);
+  arrow.addEventListener('click', ()=> {
+    const confirmButton = document.querySelector('#confirm-photo');
+    const uploadImgContaienr = document.querySelector('.add-to-gallery')
+    arrow.style.fill = 'white';
+
+    editGallary.style.display = 'grid';
+    confirmButton.style.display = 'none';
+    addPhotoButton.style.display = 'block';
+    uploadImgContaienr.style.display = 'none'
+
+  })
+
+  addPhotoButton.addEventListener('click', async () => {
+    const confirmButton = document.querySelector('#confirm-photo');
+    const uploadImgContaienr = document.querySelector('.add-to-gallery')
+    arrow.style.fill = 'black';
+
+    editGallary.style.display = 'none';
+    confirmButton.style.display = 'block';
+    addPhotoButton.style.display = 'none';
+    uploadImgContaienr.style.display = 'block'
+  });
+
+  fileInput.addEventListener('change', () => {
+  const uploadImg = document.getElementById('uploadImg');
+  const svg = document.querySelector('.img-svg')
+
+    svg.style.display = 'none';
+    uploadImg.style.display = 'block';
+    uploadImg.src = URL.createObjectURL(fileInput.files[0]);
+  });
+
+  getEditImgs(worksData, editGallary, allWorks);
 });
 
 function displayContentOnLogin() {
@@ -142,32 +177,47 @@ loginLink.addEventListener('click', () => {
   }
 });
 
-
 function getEditImgs(worksData, editGallary) {
   worksData.forEach((work) => {
     const div = document.createElement('div');
-    div.classList.add('edit-card', work.id);
+    div.setAttribute('data-custom', `${work.id}`);
+    div.classList.add('edit-card');
     div.style.backgroundImage = `url(${work.imageUrl})`;
 
-    const span = document.createElement('span');
-    span.classList.add('span-svg'); 
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('span-svg');
 
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('viewBox', '0 0 448 512');
-    svg.classList.add('deletesvg'); 
+    svg.classList.add('deletesvg');
 
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     path.setAttribute(
       'd',
       'M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z'
     );
-    
+
     svg.appendChild(path);
 
-    span.appendChild(svg);
+    deleteButton.addEventListener('click', async () => {
+      const id = div.getAttribute('data-custom');
+
+      if (confirm('Are you sure you want to delete this post?')) {
+        try {
+          const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+            method: 'DELETE',
+          });
+          console.log('Post deleted');
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      }
+    });
+
+    deleteButton.appendChild(svg);
 
     // Assuming you want to append the SVG to the body or some other element
-    div.appendChild(span);
+    div.appendChild(deleteButton);
 
     editGallary.appendChild(div);
   });
